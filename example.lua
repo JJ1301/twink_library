4744face3b0a980f4fec42ea4b2e385858cba418
JJ1301 <148344690+JJ1301@users.noreply.github.com>
2025-09-26 14:12:53 +0200

    Create example.lua

---
+++ example.lua
@@ -0,0 +1,24 @@
+local UILibrary = loadstring(game:HttpGet("https://raw.githubusercontent.com/JJ1301/gui/refs/heads/main/twink_library"))()
+
+local MainUI = UILibrary.Load("Calamity")
+local FirstPage = MainUI.AddPage("Home")
+
+local FirstLabel = FirstPage.AddLabel("Section 1")
+local FirstButton = FirstPage.AddButton("Hello", function()
+print("Hello")
+end)
+local FirstToggle = FirstPage.AddToggle("Hello", false, function(Value)
+print(Value)
+end)
+local FirstSlider = FirstPage.AddSlider("Hello", {Min = 0, Max = 255, Def = 50}, function(Value)
+print(Value)
+end)
+local FirstPicker = FirstPage.AddColourPicker("Hello", "white", function(Value)
+print(Value)
+end)
+local FirstDropdown = FirstPage.AddDropdown("Hello", {
+"Hello",
+"Goodbye"
+}, function(Value)
+print(Value)
+end)
